d738558464487e7d3470dd515ad530df5b1f3cf9
sooa1204 <jungswa124@gmail.com>
2023-12-15 18:29:28 +0900

    Merge branch 'yeonseo' of github.com:zugusdl/mes2test into sooa
    # Conflicts:
    #	TestMes2/src/hi

--- PreSpring/src/main/java/com/itwillbs/ex/MessageBean.java
+++ PreSpring/src/main/java/com/itwillbs/ex/MessageBean.java
@@ -2,8 +2,8 @@ package com.itwillbs.ex;
 
 public class MessageBean {
 
-	// 출력 기능
-	public /* static */ void sayHello(String name) {
-		System.out.println("안녕!, " + name);
+	
+	MessageBean; 
+	
 	}
 }
